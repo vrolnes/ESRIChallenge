73af4036662115e6f0a078e2588b3fe8ebde626b
Varol <>
2022-10-29 20:41:11 +0300

    Add scan rate enums

---
+++ bluetoothlibrary/src/main/java/com/example/blueetoothlibrary/constants/ScanRates.kt
@@ -0,0 +1,7 @@
+package com.example.blueetoothlibrary.constants
+
+enum class ScanRates(val duration: Long) {
+    LOW_SCAN_RATE(10000L),
+    MEDIUM_SCAN_RATE(5000L),
+    HIGH_SCAN_RATE(3000L),
+}
