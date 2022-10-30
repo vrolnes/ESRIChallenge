ea86aa8c50f2829fae093ef7ffe97e78056db537
Varol <>
2022-10-30 18:59:46 +0300

    Add scan rates

--- bluetoothlibrary/src/main/java/com/example/blueetoothlibrary/constants/ScanRates.kt
+++ bluetoothlibrary/src/main/java/com/example/blueetoothlibrary/constants/ScanRates.kt
@@ -1,7 +1,7 @@
 package com.example.blueetoothlibrary.constants
 
-enum class ScanRates(val duration: Long) {
-    LOW_SCAN_RATE(10000L),
-    MEDIUM_SCAN_RATE(5000L),
-    HIGH_SCAN_RATE(3000L),
+enum class ScanRates(val text: String, val duration: Long) {
+    LOW_SCAN_RATE("LOW", 10000L),
+    MEDIUM_SCAN_RATE("MEDIUM", 5000L),
+    HIGH_SCAN_RATE("HIGH", 3000L),
 }
